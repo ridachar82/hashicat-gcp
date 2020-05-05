01dea42b12ed2c79a325d18b1425bf0a598c387d
Ridhima <ridhima@example.com>
2020-05-05 19:54:26 +0000

    updates

--- main.tf
+++ main.tf
@@ -38,10 +38,10 @@ resource "tls_private_key" "ssh-key" {
 resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
-  labels           ={
+  tags             = {
       billable     = true
       department   = devops
-                     }  
+                    }  
   machine_type = var.machine_type
 
   boot_disk {
